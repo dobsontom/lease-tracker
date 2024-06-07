202e6b0531a78b61bb5a0eca6de04241c2cbe6dd
Tom Dobson <dobs.tx@gmail.com>
2024-06-07 16:20:35 +0100

    Add inital CTEs with joins

--- lease-tracker.sql
+++ lease-tracker.sql
@@ -11,7 +11,8 @@ WITH
             system_modstamp,
             connection_received_id,
             connection_sent_id,
-            bill_period_end_c bill_period_start_c,
+            bill_period_end_c,
+            bill_period_start_c,
             billed_amount_c,
             billing_source_c,
             billing_status_c,
@@ -24,7 +25,7 @@ WITH
                 (
                     IFNULL(external_invoice_id_c, internal_invoice_id_c)
                 )
-            ) AS invoice_number_c,
+            ) AS invoice_number,
             IFNULL(
                 billing_status_c,
                 (
@@ -33,134 +34,69 @@ WITH
                         internal_billing_status_c
                     )
                 )
-            ) AS invoice_number_c
+            ) AS billing_status
         FROM
             inm-bi.sdp_salesforce_src.invoice_c
         WHERE
             is_deleted = FALSE
     ),
+    leasing_request_c AS (
+        SELECT
+            *,
+            CAST(
+                CONCAT(
+                    CAST(CAST(lease_start_date_c AS DATE) AS STRING),
+                    "T",
+                    LEFT(CAST(lease_start_time_c AS STRING), 12)
+                ) AS DATETIME
+            ) AS start_date_of_current_lease,
+            CAST(
+                CONCAT(
+                    CAST(CAST(end_date_c AS DATE) AS STRING),
+                    "T",
+                    LEFT(CAST(lease_end_time_c AS STRING), 12)
+                ) AS DATETIME
+            ) AS end_date_of_current_lease
+        FROM
+            inm-bi.sdp_salesforce_src.leasing_request_c
+        WHERE
+            contract_number_c IS NOT NULL
+    ),
     user AS (
+        SELECT
+            id AS user_id,
+            name AS account_nanager_name,
+            division AS account_manager_division
+        FROM
+            inm-bi.sdp_salesforce_src.user
+    ),
+    leasing_request_user AS (
         SELECT
             *
         FROM
-            inm-bi.sdp_salesforce_src.user
-     )--,
-
-    -- leasing_request_c AS(
-        SELECT
-            *,
-            FORMAT_DATE('%Y-%m-%d %H:%M:%S', DATE(lease_start_date_c + " " + lease_start_time_c)) AS start_date_of_current_lease,
-            FORMAT_DATE('%Y-%m-%d %H:%M:%S', DATE(lease_end_date_c + " " + lease_end_time_c)) AS end_date_of_current_lease,
-
-        FROM
-            inm-bi.sdp_salesforce_src.leasing_request_c
-    WHERE contract_number_c IS NOT NULL
-    LIMIT 1000
-    -- )
-
-
-
-        -- SELECT
-        --     account_c,
-        --     account_manager_c,!
-        --     account_number_c,
-        --     account_period_of_first_invoice_YYYYMM_C,
-        --     approval_document_c,
-        --     approval_document_id_c,
-        --     approval_document_status_c,
-        --     approval_status_change_date_c,
-        --     assessment_comments_c,
-        --     billing_acknowledgement_of_approval_docu_c,
-        --     bupa_c,
-        --     business_unit_1_c AS business_unit,
-        --     can_be_accomodated_c,
-        --     channel_no2_c,
-        --     channel_weeks_charge_c,
-        --     channel_weeks_trustcomm_l_tac_c,
-        --     commercials_confirmed_c,
-        --     company_id_c,
-        --     consumed_amount_c,
-        --     consumed_amount_per_c,
-        --     contract_daily_rate_c,
-        --     contract_duration_days,
-        --     contract_number_c AS spp_number,
-        --     contract_progess_c,
-        --     contract_progress_comments_c,
-        --     contract_sent_to_lsp_c,
-        --     contract_signed_by_lsp_c,
-        --     contract_sla_c,
-        --     contracts_acknowledgement_of_approval_do_c,
-        --     conus_discount_trustcomm_l_tac_c,
-        --     count_c,
-        --     created_by_id,
-        --     created_by_date,
-        --     credit_check_comments_c,
-        --     credit_limit_approved_c,
-        --     credit_rejected_c,
-        --     credit_sla_c,
-        --     customer_name_c,
-        --     disaster_recovery_charge_c,
-        --     disaster_recovery_with_spectrum_c,
-        --     disaster_recovery_without_spectrum_c,
-        --     document_approval_status_changed_date_c,
-        --     due_date_c,
-        --     end_date_c,
-        --     end_user_organisation_c,
-        --     escalation_reason_c,
-        --     external_sale_profit_center_c,
-        --     forward_bandwidth_khz_c,
-        --     forward_data_rates_kbps_c,
-        --     gx_email_alert_recursion_stop_c,
-        --     host_radio3_c,
-        --     id,!
-        --     initial_request_date_c,
-        --     internal_external_formula_c,
-        --     internal_sale_profit_center_c,
-        --     is_this_application_subject_to_c,
-        --     is_deleted,
-        --     isol_gm_c,
-        --     laf_published_c,
-        --     land_or_maritime_c,
-            
-            
-
-
-            
-        --     contract_number_c AS ssp_number,!
-        --     FORMAT_DATE('%Y-%m-%d %H:%M:%S', DATE(lease_start_date_c + " " + lease_start_time_c)) AS start_date_of_current_lease,
-        --     FORMAT_DATE('%Y-%m-%d %H:%M:%S', DATE(lease_end_date_c + " " + lease_end_time_c)) AS end_date_of_current_lease,
-        --     lsp_c AS lsp,
-        --     lst_c AS lsr,
-            
-        --     land_or_maritime_c AS land_aero_maritime,
-        --     lease_service_type_c AS service_type,
-        --     lsp_c AS lsp,
-        --     lst_c AS lsr,
-        --     name AS end_customer
-
-
-        --     approval_document_id_c AS approval_docuement_id,
-        --     business_unit_1_c AS business_unit,
-        --     internal_external_formula_c AS internal_or_external,
-        --     lease_update_status_c AS lease_update_status,
-        --     retail_bill_interval_c AS retail_bill_interval,
-        --     retail_contract_value_c AS retail_contract_value,
-        --     retail_periodic_payment_amount_c AS retail_periodic_payment_amount,
-        --     retial_pricing_comments_c AS retail_pricing_comments,
-        --     revenue_recognition_c AS revenue_recognition_basis,
-        --     wholesale_bill_interval_c AS wholesale_bill_interval,
-        --     wholesale_contract_value_c AS wholesale_contract_value,
-        --     wholesale_periodic_payment_amount_c AS wholesale_periodic_payment_amount,
-        --     wholesale_pricing_comments_c AS wholesale_pricing_comments,
-
-        --     account_number_c,
-        --     invoice wholesale or retail,
-        --     invoice number,
-        --     invoice name,
-        --     invoice wholesale credit rebill,
-        --     invoice billing source,
-        --     invoice bill period start,
-        --     invoice bill period end,
-        --     invoice billing status,
-        --     invoice billed amount,
-        --     invoice created date
+            leasing_request_c lr
+            LEFT JOIN user u ON lr.account_manager_c = u.user_id
+    )
+SELECT
+    lrq.contract_number_c AS ssp_number,
+    lrq.lsp_c,
+    lrq.lsr_c,
+    lrq.name AS end_customer,
+    lrq.account_number_c,
+    lrq.land_or_maritime_c,
+    i.wholesale_or_retail_c,
+    i.invoice_number,
+    i.name AS invoice_name,
+    i.wholesale_credit_rebill_c,
+    i.billing_source_c,
+    lrq.lease_service_type_c,
+    lrq.start_date_of_current_lease,
+    lrq.end_date_of_current_lease,
+    i.bill_period_start_c AS invoice_bill_period_start,
+    i.bill_period_end_c AS invoice_bill_period_end,
+    i.billing_status_c AS invoice_billing_status,
+    i.billed_amount_c AS invoice_billed_amount,
+    i.created_date AS invoice_created_date
+FROM
+    leasing_request_user lrq
+    JOIN invoice_c i ON lrq.id = i.leasing_request_c
