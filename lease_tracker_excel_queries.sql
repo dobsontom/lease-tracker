1805af39291259a825381c7b2fed6af6d2c53dcd
Tom Dobson <dobs.tx@gmail.com>
2024-09-27 13:10:40 +0100

    Add lease tracker excel queries to populate the various tabs of data used in Tracker and Pivot tabs

---
+++ lease_tracker_excel_queries.sql
@@ -0,0 +1,34 @@
+-- Populates Raw Data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_raw_data`;
+
+-- Populates Billing Data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_billing_data`;
+
+-- Populates Wholesale External data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_wholesale_external_data`;
+
+-- Populates Segovia data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_segovia_data`;
+
+-- Populates Retail data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_retail_data`;
+
+-- Populates GX Wholesale External data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_gx_wholesale_external_data`;
+
+-- Populates GX Segovia data tab
+SELECT
+    *
+FROM `inm-iar-data-warehouse-dev.lease_tracker.lease_tracker_gx_segovia_data`;
