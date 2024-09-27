5ecb0b72f0553dfb8d7414696840b0e50a793909
Tom Dobson <dobs.tx@gmail.com>
2024-09-27 13:09:20 +0100

    Update base table reference name to call_off_tracker_base

--- call_off_tracker_excel_queries.sql
+++ call_off_tracker_excel_queries.sql
@@ -1,13 +1,16 @@
+-- Populates Raw Data tab
 SELECT
     *
-FROM `inm-iar-data-warehouse-dev.lease_tracker.call_off_tracker`;
+FROM `inm-iar-data-warehouse-dev.lease_tracker.call_off_tracker_base`;
 
+-- Populates Retail Data tab
 SELECT
     *
-FROM `inm-iar-data-warehouse-dev.lease_tracker.call_off_tracker`
+FROM `inm-iar-data-warehouse-dev.lease_tracker.call_off_tracker_base`
 WHERE rtl_flag = 1;
 
+-- Populates Wholesale Data tab
 SELECT
     *
-FROM `inm-iar-data-warehouse-dev.lease_tracker.call_off_tracker`
+FROM `inm-iar-data-warehouse-dev.lease_tracker.call_off_tracker_base`
 WHERE whs_flag = 1;
